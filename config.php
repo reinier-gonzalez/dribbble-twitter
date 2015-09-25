6a5535d9fc47e89b3bb5bc0e8372b9cb95e69f7f
Reinier G Menendez <reinier@soft4good.com>
2015-09-25 02:28:39 -0300

    emptying config and making progress

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	
 	// Dribbble
-  define( 'DRIBBBLE_CLIENT_ID', 	 'b4a1e046db706ba9a354ed1e0c3f8a31d4354aca3e408b4852bdd751360d135f' );
-  define( 'DRIBBBLE_REDIRECT_URL', 'http://baymonsters.com/dribbble-twitter/' );
+  define( 'DRIBBBLE_CLIENT_ID', 	 '' );
+  define( 'DRIBBBLE_REDIRECT_URL', '' );
 
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php
 	require 'vendor/autoload.php';
+  require 'classes/baseapimodel.class.php';
+
   require 'config.php';
 
+  // let's do it
   session_start();
 
   // authenticate on dribbble
-  if ( isset($_GET['code'] ) ) // is there an access code?
+  if ( isset( $_GET['code'] ) ) // is there an access code?
   {
     // ok let's check it
-    $sCSRFToken =
-    if ( iss)
+    $sCurrentState = $_SESSION['state'];
+    if ( isset( $_GET['state'] ) && $_GET['state'] == $sCurrentState )
+    {
+      // cool, let's exchange it
+      $sCode = $_GET['code'];
+      //
+      https://dribbble.com/oauth/token
+      BaseApiModel::post( 'https://dribbble.com/oauth/token',
+        array(
+          'client_id' => DRIBBBLE_CLIENT_ID,
+          'client_secret' => DRIBBB
+        )
+      ); . DRIBBBLE_CLIENT_ID . '&client_secret=' . DRIBBBLE_CLIENT_ID . '&code=' . $sCode );
+      $oDribbbleClient = new Dribbble\Api\Client();
+      $oDribbbleClient->
 
+    }
+    else die( "Damn Hackers!" );
   }
   else
   {
-    $sCSRFToken = $_SESSION['state'] = uniqid( "", true );
-    header( 'https://dribbble.com/oauth/authorize?client_id=' . DRIBBBLE_CLIENT_ID . '&state=' . $sCSRFToken );
+    $sState = $_SESSION['state'] = uniqid( "", true );
+    header( 'https://dribbble.com/oauth/authorize?client_id=' . DRIBBBLE_CLIENT_ID . '&state=' . $sState );
   }
 
 
