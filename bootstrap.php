2c1ffe52553eb5a5159f999eef9f3b188c978fda
Reinier G Menendez <reinier@soft4good.com>
2015-09-26 15:34:45 -0300

    removing debuggin stuff

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,6 @@
 	require 'vendor/autoload.php';
   
   require 'classes/baseapimodel.class.php';
-  require 'classes/debug.class.php';
 
   require 'config.php';
   ////////////////////////////////////////////////////////////////////////
